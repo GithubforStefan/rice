8f07a31a783987a98c98fd506d91181d5ac8776a
Stefan Kraus <stefan.kraus@posteo.eu>
2020-03-10 21:22:14 +0100

    Initial commit

---
+++ src/Verdoppelt.java
@@ -0,0 +1,13 @@
+public class Verdoppelt {
+
+public static void main(String[] args) {
+    int i;
+    double reiskorn = 1;
+
+    for(i=1;i<=64;i++){
+        reiskorn = reiskorn * 2;
+        }
+    System.out.println(reiskorn);
+}
+
+}
